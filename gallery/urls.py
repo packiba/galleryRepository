cae1a343e99fb46e7216ed8a52651bc41803a94f
Pavel Kiba <packiba@gmail.com>
2019-06-09 22:29:08 +0300

    добавил стили css

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,13 +1,6 @@
 from django.urls import path
 from . import views
-from django.conf import settings
-#
-# if settings.DEBUG:
-#     from django.conf.urls.static import static
-#     from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', views.piclist, name='pictures'),
 ]
-# urlpatterns += staticfiles_urlpatterns()
-# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
